07837f677fceb5cbe4dfecb1a0ba89b5851b0315
Eric Gibertson <eric_gilbertson@yahoo.com>
2022-08-28 21:01:35 -0700

    add archive file check

--- show_utils.py
+++ show_utils.py
@@ -1,5 +1,30 @@
-import os, random, datetime
+import os, random, datetime, urllib.request, json
 from random import randint
+
+def get_time_from_zookeeper(time_str):
+    hour = float(time_str[0:2]) + float(time_str[2:4])/60.0
+    return hour
+
+def get_show_info(showdate):
+    day_info = None
+    url = 'https://zookeeper.stanford.edu/api/v1/playlist?filter[date]=' + showdate.strftime('%Y/%m/%d')
+    response = urllib.request.urlopen(url)
+    if response.status != 200:
+        return None
+
+    day_info = json.loads(response.read())
+    start_hour = showdate.hour
+    for show in day_info['data']:
+        attributes = show['attributes']
+        time_ar = attributes['time'].split('-')
+        show_start = get_time_from_zookeeper(time_ar[0])
+        show_end = get_time_from_zookeeper(time_ar[1])
+        show_end = show_end + 24 if show_end < show_start else show_end
+
+        if start_hour >= show_start  and start_hour < show_end:
+            return show
+
+    return None
 
 def random_datetime(start, end):
     rand_seconds = random.randint(0, int((end - start).total_seconds()))
@@ -9,7 +34,10 @@ def random_datetime(start, end):
 def get_show_file(safe_harbor):
     FILE_ROOT = '/Volumes/Public/kzsu-aircheck-archives/'
     start_date = datetime.datetime(2012, 2, 16)
-    end_date = datetime.datetime(2022, 8, 1)
+    end_date = datetime.datetime.now() - datetime.timedelta(hours=24)
+
+
+    #return (datetime.datetime(2022, 3, 3, 12), 'test_file')   #########
 
     idx = 0
     while idx < 1000:
@@ -18,19 +46,26 @@ def get_show_file(safe_harbor):
         showfile = showdate.strftime('%Y/%b/%d/kzsu-%Y-%m-%d-%H00.mp3')
 
         hour = showdate.hour
-        if hour < 6 or not safe_harbor and hour >= 22:
+        if hour < 7 or not safe_harbor and hour >= 22:
             continue
 
         file_path = FILE_ROOT + showfile
         if os.path.exists(file_path):
-            return file_path
+            return (showdate, file_path)
 
-        print("skip")
-
-    return None
+    return (None, None)
 
 if __name__ == '__main__':
     for i in range(10):
-        file = get_show_file(False)
-        print("show file: {}".format(file))
+        (showdate, file) = get_show_file(False)
+        if showdate:
+            showinfo = get_show_info(showdate)
+            title = 'Unknown'
+            if showinfo:
+                attrs = showinfo['attributes']
+                title = attrs['name']
+
+            print("show {}, {}, {}".format(showdate, file, title))
+        else:
+            print("No file")
 
